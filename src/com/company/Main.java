4f3cf8d0a9475866709271931c5edb42118916c0
Kairatbek-hub <kairatbek.shabotoev@gmail.com>
2022-09-23 17:05:32 +0600

    Revert "Added field home number"
    This reverts commit 9181401d

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,7 +13,5 @@ public class Main {
     public void greeting(){
         System.out.println("Hi");
         System.out.println("Hi2");
-        System.out.println("Hi3");
-        System.out.println("Hi4");
     }
 }
--- src/com/company/Test2.java
+++ src/com/company/Test2.java
@@ -3,6 +3,4 @@ package com.company;
 public class Test2 {
     private String name;
     private String address;
-    private Long homeNumber;
-    private int age;
 }
